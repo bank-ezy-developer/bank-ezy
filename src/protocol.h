8aea338207ed828af8b046a87f89eb3a1fc97781
Bank-ezy <42500450+bank-ezy-developer@users.noreply.github.com>
2018-08-21 19:02:22 +0100

    Update protocol.h
    Updated conflicting port for main net from 20188 to 20199

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 33813 : 20188;
+    return testnet ? 33813 : 20199;
 }
 
 
